cbf5fb6eccbc0655460275608480b018a6297235
Jose Luciano Guzman Santiago <jguzman@dcanje.com>
2024-07-09 11:17:04 -0500

    test nfc

--- src/App.js
+++ src/App.js
@@ -27,8 +27,35 @@ const  read = () =>{
           // eslint-disable-next-line no-undef
             const ndef = new NDEFReader();
             try {
-                const tarjeta = await read();
-                setOutput(`Tarjeta leída: ${tarjeta}`);
+                //const tarjeta = await read();
+                read().then((tarjeta) => {
+                  setOutput(tarjeta);
+                }
+                );
+                setOutput("Esperando una etiqueta NFC...");
+                /* ndef.onreading = event => {
+                    const message = event.message;
+                    let newOutput = '';
+                    for (const record of message.records) {
+                        const textDecoder = new TextDecoder(record.encoding);
+                        newOutput += `Tipo de registro: ${record.recordType}\n`;
+                        newOutput += `Tipo de medio: ${record.mediaType}\n`;
+                        newOutput += `Datos: ${textDecoder.decode(record.data)}\n`;
+                    }
+                    setOutput(newOutput);
+                }; */
+                ndef.scan().then(() => {
+                  console.log("Scan started successfully.");
+                  ndef.onreadingerror = (event) => {
+                    console.log("Error! Cannot read data from the NFC tag. Try a different one?");
+                  };
+                  ndef.onreading = (event) => {
+                    const message = event.message;
+                    alert("NDEF message read." + message);
+                  };
+                }).catch(error => {
+                  console.log(`Error! Scan failed to start: ${error}.`);
+                });
             } catch (error) {
               alert(error.message);
                 setOutput(`Error al iniciar la lectura NFC: ${error}`);
